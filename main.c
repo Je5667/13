0eb1b8c446e4ba7571a08309b6096f1bdf824a00
Je5667 <113067745+Je5667@users.noreply.github.com>
2022-11-29 01:00:40 +0900

    1-2

--- main.c
+++ main.c
@@ -7,6 +7,12 @@ struct student {
 	double grade;
 };
 
-int main (void) {
-	struct student s1 = {24, "각자이름", 4.3};
+void main (void) {
+	struct student s = {24, "각자이름", 4.3};
+	s.ID = 123456;
+	strcpy(s.name, "Juyeop");
+	s.grade = 4.2;
+	printf("ID : %d\n", s.ID);
+	printf("name : %s\n", s.name);
+	printf("grade : %f\n", s.grade);
 }
